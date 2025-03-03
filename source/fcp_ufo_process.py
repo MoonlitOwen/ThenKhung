0264c2ee1904fd59d7e931ddf98be66aae63e0b0
Aaron Bell <aaron@sajatypeworks.com>
2025-03-02 16:48:31 -0800

    Further updates
    Added comments to the UFO processing script, added BASE table support. Also resolved overlapping and broken outlines in some glyphs.
    
    Not sure why it is fully replacing the UFOs, but here we are.

--- source/fcp_ufo_process.py
+++ source/fcp_ufo_process.py
@@ -14,7 +14,7 @@ SOURCE = Path("source/")
 for source in SOURCE.glob("*.ufo"):
     UFO = Path(shutil.copytree(str(source),str(source).replace("source/","source/temp/"),dirs_exist_ok=True))
 
-	# Clearing BOM encoding. REMOVE IF 
+	# Clearing BOM encoding in older versions of FCP. 
     for file in UFO.rglob("*.*"):
         try:
             with open(str(file), 'rb') as f:
@@ -31,7 +31,7 @@ for source in SOURCE.glob("*.ufo"):
 
 
 
-	# Adding missing platform ID
+	# Older FCP does not add the encodingID properly
     fontInfo = UFO / "fontinfo.plist"
 
     plist = open(fontInfo, 'r')
@@ -46,7 +46,7 @@ for source in SOURCE.glob("*.ufo"):
     with open (fontInfo, 'w') as plistWriter:
         plistWriter.write (content_new)
 
-	# Correcting invalid feature file data
+	# Correcting invalid feature file data in older FCP output
     features = UFO / "features.fea"
 
     fea = open(features, 'r')
@@ -62,12 +62,42 @@ for source in SOURCE.glob("*.ufo"):
     with open (features, 'w') as feaWriter:
         feaWriter.write(content_new)
 
+    # Now on to modify the UFO some
 
     font = ufoLib2.Font.open(UFO)
+    print ("Removing NID16/17 from UFO source") #This is added by FCP incorrectly. 
     name_records = font.info.openTypeNameRecords
     updated_name_records = [
         record for record in name_records if record.get("nameID") not in [16, 17]
     ]
     font.info.openTypeNameRecords = updated_name_records
+
+    base_table_content = """
+table BASE {
+HorizAxis.BaseTagList   icfb    icft    ideo    romn;
+HorizAxis.BaseScriptList  
+    DFLT    ideo  	-97   851  -123     0,
+    hani    ideo   	-97   851  -123     0,
+    kana    ideo   	-97   851  -123     0,
+    hang    ideo   	-97   851  -123     0,
+    latn    romn   	-97   851  -123     0;
+
+VertAxis.BaseTagList    icfb    icft    ideo    romn;
+VertAxis.BaseScriptList
+    DFLT    ideo    26   974     0   123,
+    hani    ideo    26   974     0   123,
+    kana    ideo    26   974     0   123,
+    hang    ideo    26   974     0   123,
+    latn    romn    26   974     0   123;
+} BASE;
+"""
+
+    features = font.features.text if font.features.text else ""
+    if "table BASE" in features:
+        pass
+    else:
+        print("Adding BASE table to features.fea.")
+        features += f"\n\n{base_table_content}\n"
+        font.features.text = features
 
     font.save(UFO,overwrite=True)
