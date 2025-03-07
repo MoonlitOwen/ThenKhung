a450b09e34c041f1f1f43b625804e154cab8544e
Aaron Bell <aaron@sajatypeworks.com>
2025-03-06 19:50:37 -0800

    Update build.sh
    making sure that the venv is active when building

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
 if [ -d "fonts/" ]; then rm -rf "fonts"; fi
 if [ -d "source/temp/" ]; then rm -rf "source/temp"; fi
 mkdir "source/temp"
