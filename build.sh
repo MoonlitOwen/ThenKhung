4617f900486d13d828ec2170b4a4876f99704f04
Aaron Bell <aaron@sajatypeworks.com>
2025-03-06 19:43:46 -0800

    activating venv for the python scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 if [ -d "fonts/" ]; then rm -rf "fonts"; fi
 if [ -d "source/temp/" ]; then rm -rf "source/temp"; fi
 mkdir "source/temp"
-python3 source/scripts/extract.py
-python3 source/fcp_ufo_process.py
+. venv/bin/activate; python3 source/scripts/extract.py
+. venv/bin/activate; python3 source/fcp_ufo_process.py
 gftools builder source/config.yaml
 # fontbakery check-googlefonts fonts/ttf/UoqMunThenKhung-Regular.ttf -l WARN --ghmarkdown results.md
